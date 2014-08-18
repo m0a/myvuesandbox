6f8f5d57bde04b85e4a85d11cb7f6c67496b5db7
makoto abe <abe00makoto@gmail.com>
2014-08-18 17:10:50 +0900

    tutorial用の記述を追加

--- myfirebase/app.js
+++ myfirebase/app.js
@@ -1,23 +1,38 @@
 var baseURL = 'https://m0a-s-sandbox.firebaseio.com/',
-    Chats   = new Firebase(baseURL + 'chats')
+    Chats   = new Firebase(baseURL + 'chats');
 
 Chats.on('child_added', function (snapshot) {
-    var message = snapshot.val()
-    message.id = snapshot.name()
-    app.chats.push(message)
+    var message = snapshot.val();
+    message.id = snapshot.name();
+    app.chats.push(message);
 })
 
 Chats.on('child_changed', function (snapshot) {
-  var id = snapshot.name()
+  var id = snapshot.name();
   console.log('child_changed:'+snapshot.name());
   app.chats.some(function(message) {
         if (message.id === id) {
-            message.body = snapshot.val().body
-            return true
+            message.body = snapshot.val().body;
+            return true;
         }
     })
 })
 
+var authClient = new FirebaseSimpleLogin(Chats, function(error, user) {
+  if (error !== null) {
+    console.log("Login error:", error);
+  } else if (user !== null) {
+
+    console.log("User authenticated with Firebase:", user);
+    app.newMessage.name = user.uid
+  } else {
+    console.log("User is logged out");
+  }
+});
+// Log user in anonymously
+var login = authClient.login("anonymous", {
+  rememberMe: true
+})
 
 
 var app = new Vue({
